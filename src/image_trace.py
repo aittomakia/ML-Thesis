35c256ff564450cdade38344579e63ef49b23ca5
ariaittomaki <ari.aittomaki@sc5.io>
2018-06-01 13:20:20 +0300

    modified regocnition functionality

--- src/image_trace.py
+++ src/image_trace.py
@@ -9,7 +9,6 @@ https://dsp.stackexchange.com/questions/2564/opencv-c-connect-nearby-contours-ba
 '''
 
 def is_close(rect1,rect2):
-
     max_x1, min_x1, max_y1, min_y1 = rect1[0]+rect1[2],rect1[0],rect1[1]+rect1[3],rect1[1]
     max_x2, min_x2, max_y2, min_y2 = rect2[0]+rect2[2],rect2[0],rect2[1]+rect2[3],rect2[1]
 
@@ -53,33 +52,13 @@ def intersection_rect(a,b):
 def sortByX(item):
     return item[1][0]
 
-# the real deal
 def find_components(img):
 
-    # convert to gray and binary threshold
-    # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    gray = img
-    _,thresh = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
-
-    # let's find the contours
-    mser=True
-    if mser:
-        # use this to play with MSER params:
-        # https://docs.opencv.org/trunk/d3/d28/classcv_1_1MSER.html#a136c6b29fbcdd783a1003bf429fa17ed
-        #mser = cv2.MSER_create(5, 60, 14400, 0.25, 0.2, 200, 1.01, 0.003, 5)
-        mser = cv2.MSER_create()
-
-        # Hey, wait! Why are you using the thresholded image here???
-        # First because it is much, much faster. Second because it gives me better results.
-        # So you may ask: why are you using MSER in the first place? Because this "wrong"
-        # combo is giving me the best results so far. Advices are welcome.
-        regions = mser.detectRegions(thresh)
-        hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
-        contours = hulls
-    else:
-        thresh = cv2.bitwise_not(img) # wants black bg
-        im2,contours,hier = cv2.findContours(thresh,cv2.RETR_EXTERNAL,2)
-
+    _,thresh = cv2.threshold(img, 130, 255, cv2.THRESH_BINARY)
+    mser = cv2.MSER_create()
+    regions = mser.detectRegions(thresh)
+    hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
+    contours = hulls
 
     # Found their bounding boxes (as rectangles and as contours, just to make life easier later)
     # and put them all together
@@ -89,14 +68,6 @@ def find_components(img):
         bboxes.append((cr,rect,c))
 
     contours = bboxes
-
-    '''
-    base_contours=list(c[0] for c in bboxes)
-    cv2.drawContours(img, base_contours, -1, (0,0,255), 1)
-    cv2.imshow('base contours', img)
-    cv2.waitKey(0)
-    '''
-
     # compare each box to each other and, if they are close, assign them the same group number
     # Each elements in status correspond to a box and the value is the group it belongs to
     contours = sorted(contours, key=sortByX)
@@ -148,12 +119,8 @@ def find_components(img):
 # and the fragments ignored by the MSER
 def applyMask(img, contours):
 
-    # grayscale and negate
-    # img = cv2.imread(img,0)
     img = cv2.bitwise_not(img)
-
     dim = np.shape(img)
-
     mask = np.zeros((dim[0], dim[1]), dtype=np.float)
     CV_FILLED=-1
     cv2.drawContours(mask, contours, -1, (1,1,1), CV_FILLED)
@@ -168,10 +135,7 @@ def applyMask(img, contours):
 
 def trace(img_path_and_name, image_name ,UPLOAD_FOLDER):
 
-    start = time.time()
-
     img = cv2.imread(img_path_and_name, 0)
-
     height, width = img.shape[:2]
     max_height = 600
     max_width = 600
@@ -187,8 +151,6 @@ def trace(img_path_and_name, image_name ,UPLOAD_FOLDER):
 
     unified = find_components(img)
 
-    print("Elapsed: ", (time.time()-start), "s")  # 0.078s for an 800x600 img
-
     # extract the contours
     unified_cnt = list(c[0] for c in unified)
 
@@ -202,12 +164,5 @@ def trace(img_path_and_name, image_name ,UPLOAD_FOLDER):
         filenames.append(f_name)
         cv2.imwrite(str(str(UPLOAD_FOLDER) + '/temp/{}.png'.format(str(image_name + "-" + str(i)))), img[y:y+h,x:x+w])
         cv2.rectangle(draw_img, (x,y), (x+w,y+h), (255, 0, 0), 1)
-    # cv2.imshow('result', draw_img)
-    # # optionally apply a mask
-    # result = applyMask(img, unified_cnt)
-    # result = cv2.bitwise_and(img, img, unified_cnt)
-    # cv2.imwrite("result.png", result);
 
     return filenames
-
-    # cv2.destroyAllWindows()
